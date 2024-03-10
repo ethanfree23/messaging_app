f1755ae42fc8a1ba163049b74d1a06ce8955b002
Ethan Freeman <92946781+ethanfree23@users.noreply.github.com>
2024-03-10 19:55:52 +0000

    tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="">
-      <h1>Hello World!</h1>
+    <div className="flex border border-black-3">
+      <h1 className="text-3xl font-bold underline text-blue-300">Hello World!</h1>
     </div>
   );
 }
