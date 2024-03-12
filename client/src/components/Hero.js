11624b5613798c22fe931472e9434d307f31cadb
Ethan Freeman <92946781+ethanfree23@users.noreply.github.com>
2024-03-12 03:33:21 +0000

    hero fixed

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,16 +1,4 @@
-// import { useState } from 'react'
-// import { Dialog } from '@headlessui/react'
-// import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-
-// const navigation = [
-//   { name: 'Product', href: '#' },
-//   { name: 'Features', href: '#' },
-//   { name: 'Marketplace', href: '#' },
-//   { name: 'Company', href: '#' },
-// ]
-
-export default function Hero() {
-  // const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+function Hero() {
 
   return (
     <div className="bg-white">
@@ -132,3 +120,5 @@ export default function Hero() {
     </div>
   )
 }
+
+export default  Hero;
