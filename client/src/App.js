841529e31677df39a4427fcdde49a1481e0828ab
Ethan Freeman <92946781+ethanfree23@users.noreply.github.com>
2024-03-27 21:32:20 +0000

    copy

--- client/src/App.js
+++ client/src/App.js
@@ -293,7 +293,7 @@ function App() {
         </div> */}
 
         {/* Feature section */}
-        <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-56 lg:px-8">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl text-center">
             <h2 className="text-base font-semibold leading-7 text-indigo-400">Get it done with Tee</h2>
             <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
